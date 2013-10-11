8ea91431b91a4aa18824e9ed6a1a58a63374ae8b
Vasily Loginevskiy <yeti.or@gmail.com>
2013-10-11 05:24:03 +0400

    Frontend: Add simple navigation between students.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,13 +6,22 @@ app.controller('StudentsCtrl', function(Students,$scope,$state) {
     });
 
     $scope.showStudent = function(index){
-        console.log(index);
         $state.go('student',{studentId:index});
     };
 });
 
 app.controller('StudentCtrl',function(Students,$scope,$state,$stateParams){
+    var index = $stateParams.studentId;
+
     Students.query(function(data){
-        $scope.student = data[$stateParams.studentId];
+        $scope.student = data[index];
     });
+
+    $scope.nextOne = function(){
+        $state.go('student',{studentId:++index});
+    };
+
+    $scope.prevOne = function(){
+        $state.go('student',{studentId:--index});
+    };
 });
