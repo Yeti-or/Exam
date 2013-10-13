b8cbc2de7d10ada89f2785ef6e52ced79150b382
Vasily Loginevskiy <yeti.or@gmail.com>
2013-10-13 22:14:38 +0400

    Frontend: Improve bower components of app.

--- Gruntfile.js
+++ Gruntfile.js
@@ -246,8 +246,6 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
-            'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'styles/fonts/*'
           ]
